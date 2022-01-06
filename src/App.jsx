3501478160739e5ef7533b50c1a767376157ec06
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-01-06 12:14:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.scss";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Main() {
+  return <div>Body</div>;
+}
+
+export default Main;
