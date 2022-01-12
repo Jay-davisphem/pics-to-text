f0428c77513636894fac1c47c9ec326b91c3d921
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-01-12 18:59:22 +0100

    continue project on a laptop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import './App.scss'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import "./App.scss";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Main />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
