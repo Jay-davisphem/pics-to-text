e8a75a17c2c2af58480fe6c73855c9fde9be5280
David Oluwafemi Joshua <57117270+Jay-davisphem@users.noreply.github.com>
2022-01-29 21:23:34 +0100

    Update FinalTextplace.jsx
    Remove the testing 'cool'

--- src/components/main/FinalTextplace.jsx
+++ src/components/main/FinalTextplace.jsx
@@ -1,7 +1,7 @@
 import { nanoid } from "nanoid";
 const FinalTextPlace = ({ data }) => {
   return (
-    <>{data && data.map((datum) => <p key={nanoid()}>{datum.text}COOL</p>)}</>
+    <>{data && data.map((datum) => <p key={nanoid()}>{datum.text}</p>)}</>
   );
 };
 
